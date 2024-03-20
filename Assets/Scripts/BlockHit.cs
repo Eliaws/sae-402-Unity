2a4d866fa7ebbca362c78a6bc522a0969d92720a
elias ouabala <e-eouabala@etu.loc>
2024-03-20 15:36:06 +0100

    Mise à jour des Box / Apparition et récupération des pommes

--- Assets/Scripts/BlockHit.cs
+++ Assets/Scripts/BlockHit.cs
@@ -10,6 +10,17 @@ public class BlockHit : MonoBehaviour
 
     public int maxHits = -1;
     private bool isAnimating = false;
+    public SpriteRenderer sr;
+    public bool isHidden = false;
+    public PlatformEffector2D platformEffector2D;
+    public GameObject collectiblePrefab;
+
+    private void Awake(){
+        platformEffector2D.enabled = isHidden;
+        if(isHidden){
+            sr.color = Color.clear;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player")&& !isAnimating && maxHits !=0) {
@@ -25,10 +36,29 @@ public class BlockHit : MonoBehaviour
     }
 
     IEnumerator Hit() {
+        platformEffector2D.enabled = false;
+        sr.color = Color.white;
         maxHits = maxHits-1;
         isAnimating = true;
         Vector3 endPosition = transform.position + Vector3.up * 0.5f;
         yield return transform.MoveBackAndForth(endPosition);
+
+        if(collectiblePrefab != null){
+            GameObject collectible = Instantiate(
+                collectiblePrefab,
+                transform.position,
+                Quaternion.identity);
+            Collectible collec = collectible.GetComponent<Collectible>();
+            collec.canBeDestroyedOnContact = false;
+
+            Vector3 collectibleEndPosition = (
+                collectible.transform.localPosition + Vector3.up * 1.5f);
+            yield return collectible.transform.MoveBackAndForth(
+                collectibleEndPosition
+            );
+            collec.Picked();
+        }
+
         isAnimating = false;
 
     }
